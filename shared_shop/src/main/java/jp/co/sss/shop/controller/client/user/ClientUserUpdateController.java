50b220f3c92c2a86d9ce74a7eb588fbd15b8e0d8
ykamimu <ykamimu@C010423151.zipangu.cac.co.jp>
2026-06-09 12:13:55 +0900

    add:会員情報変更の確認処理、完了処理を追加した

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
@@ -1,5 +1,6 @@
 package jp.co.sss.shop.controller.client.user;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -72,7 +73,7 @@ public class ClientUserUpdateController {
 		//リクエストスコープに保存
 		model.addAttribute("userForm", userForm);
 
-		// 入力画面のHTMLを表示
+		// 変更入力画面表示
 		return "client/user/update_input";
 	}
 
@@ -94,6 +95,78 @@ public class ClientUserUpdateController {
 
 		//GETメソッドへリダイレクト
 		return "redirect:/client/user/update/check";
+	}
+
+	/**
+	 * 変更確認画面の表示処理
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(path = "/client/user/update/check", method = RequestMethod.GET)
+	public String checkUpdateGet(Model model) {
+		//セッションスコープから入力フォーム情報を取得
+		UserForm userForm = (UserForm) session.getAttribute("updateUserForm");
+
+		// セッションが切れていた場合は入力画面へ戻す
+		if (userForm == null) {
+			return "redirect:/client/user/update/input";
+		}
+
+		//入力フォーム情報をリクエストスコープに保存
+		model.addAttribute("userForm", userForm);
+
+		//変更確認画面表示
+		return "client/user/update_check";
+	}
+
+	/**
+	 * 登録ボタン押下時処理
+	 * @return
+	 */
+	@RequestMapping(path = "/client/user/update/complete", method = RequestMethod.POST)
+	public String completeUpdatePost() {
+		UserForm userForm = (UserForm) session.getAttribute("updateUserForm");
+		if (userForm == null) {
+			return "redirect:/client/user/update/input";
+		}
+
+		//DBから元のデータを取得し変更した項目を上書き
+		User user = userRepository.getReferenceById(userForm.getId());
+		user.getName();
+		user.setEmail(userForm.getEmail());
+		user.setPassword(userForm.getPassword());
+		user.setName(userForm.getName());
+		user.setPostalCode(userForm.getPostalCode());
+		user.setAddress(userForm.getAddress());
+		user.setPhoneNumber(userForm.getPhoneNumber());
+
+		// DB更新実施
+		userRepository.save(user);
+
+		// セッションスコープの入力フォーム情報削除
+		session.removeAttribute("updateUserForm");
+
+		//セッションスコープの会員情報を更新
+		UserBean loginUser = (UserBean) session.getAttribute("user");
+		if (loginUser != null) {
+			//最新の情報をコピー
+			BeanUtils.copyProperties(user, loginUser);
+			// セッションを最新情報で上書き
+			session.setAttribute("user", loginUser);
+		}
+
+		//変更完了画面表示処理にリダイレクト
+		return "redirect:/client/user/update/complete";
+	}
+
+	/**
+	 * 変更完了画面の表示処理
+	 * @return
+	 */
+	@RequestMapping(path = "/client/user/update/complete", method = RequestMethod.GET)
+	public String completeUpdateGet() {
+		// ・登録完了画面表示
+		return "client/user/update_complete";
 	}
 
 }
