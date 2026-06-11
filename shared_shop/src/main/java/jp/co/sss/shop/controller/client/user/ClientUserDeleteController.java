d9d2b18ca5d53bf3c29c9c09dc8291672711fe54
kakawaba <kakawaba@C010423173.zipangu.cac.co.jp>
2026-06-11 09:56:25 +0900

    update:会員削除画面のコメント追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserDeleteController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserDeleteController.java
@@ -23,54 +23,87 @@ public class ClientUserDeleteController {
 	HttpSession session;
 
 	// 退会確認画面表示
+	/**
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/delete/check", method = RequestMethod.POST)
 	public String userCheck() {
 
+		//セッションからログイン中の会員情報を取得
 		UserBean loginUser = (UserBean) session.getAttribute("user");
+
+		//DBから未削除の会員情報を取得
 		User user = repository.findByIdAndDeleteFlag(
 				loginUser.getId(),
 				Constant.NOT_DELETED);
 
+		//画面表示用Beanへコピー
 		UserBean userForm = new UserBean();
 		BeanUtils.copyProperties(user, userForm);
 
+		//セッションスコープへ登録
 		session.setAttribute("userForm", userForm);
 
+		//GETメソッドへリダイレクト
 		return "redirect:/client/user/delete/check";
 	}
 
 	// 退会確認画面
+	/**
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/delete/check", method = RequestMethod.GET)
-	public String userCheck2(Model model) {
+	public String userDeletecheck(Model model) {
 
+		//セッションから会員情報を取得
 		UserBean userForm = (UserBean) session.getAttribute("userForm");
 
+		//リクエストスコープへ登録
 		model.addAttribute("userForm", userForm);
 
+		//退会確認画面を表示
 		return "client/user/delete_check";
 	}
 
-	// 退会処理
+	//会員退会処理
+	/**
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/delete/complete", method = RequestMethod.POST)
 	public String userComplete() {
 
+		//セッションから会員情報を取得
 		UserBean userForm = (UserBean) session.getAttribute("userForm");
 
+		//DBから対象会員情報を取得
 		User user = repository.findById(userForm.getId()).get();
 
+		//削除フラグ削除済みに変更
 		user.setDeleteFlag(Constant.DELETED);
 
+		//会員情報を更新
 		repository.save(user);
 
+		//買い物かご情報を削除
+		session.removeAttribute("basketBeans");
+
+		//セッション内の会員情報を削除
 		session.removeAttribute("userForm");
+
+		//セッションを破棄し未ログイン状態にする
 		session.invalidate();
 
+		//退会完了画面へリダイレクト
 		return "redirect:/client/user/delete/complete";
 	}
 
 	// 退会完了画面
+	/**
+	 * @return
+	 */
 	@RequestMapping(path = "/client/user/delete/complete", method = RequestMethod.GET)
-	public String userComplete2() {
+	public String userDeleteComplete() {
 
 		return "client/user/delete_complete";
 	}
